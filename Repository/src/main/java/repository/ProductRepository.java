2966e7fd46f9269d184f4fd8d5dd418e59311ea9
Roszinecz Norbert <norbert.roszinecz@stud.ubbcluj.ro>
2022-12-14 17:56:30 +0200

    PC-30 delete favorites

--- Repository/src/main/java/repository/ProductRepository.java
+++ Repository/src/main/java/repository/ProductRepository.java
@@ -8,8 +8,6 @@ import java.util.List;
  * Custom interface for the product repository
  */
 public interface ProductRepository extends Repository<Product>{
-    void test();
-
     List<Product> getFavoritesByUid(int uid) throws Exception;
 
     List<Product> getWatchlistByUid(int uid) throws Exception;
@@ -21,5 +19,7 @@ public interface ProductRepository extends Repository<Product>{
     void addToFavorites(int uid, Product p) throws Exception;
 
     void addToWatchlist(int uid, Product p) throws Exception;
+
+    void deleteFromFavorites(int uid, int id) throws Exception;
 }
 
